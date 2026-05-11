323f3050c8f557b46ba4b3952ddd5572c41bbe5c
Vinicius Medina <vinicius.fernandes.medina@gmail.com>
2026-05-10 23:42:04 -0300

    create repository

--- lib/domain/entities/session_entity.dart
+++ lib/domain/entities/session_entity.dart
@@ -1,10 +1,12 @@
 class SessionEntity {
+  final String userId;
   final String token;
   final String? refreshToken;
   final DateTime? expiresAt;
   final String? devices;
 
   SessionEntity({
+    required this.userId,
     required this.token,
     this.refreshToken,
     this.expiresAt,
